1a67f972f949e0e5c1362403c69314076e35dec9
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-12-20 17:53:51 -0300

    alteração na descrição da página de login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,6 +11,9 @@ const Login = () => {
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
         </p>
       </div>
 
