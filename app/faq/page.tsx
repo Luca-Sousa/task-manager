4c919da3b0d4c77629c04f88cd492d5a2f627e66
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-12-20 15:28:50 -0300

    initial commit

---
+++ app/faq/page.tsx
@@ -0,0 +1,5 @@
+const Faq = () => {
+    return ( <h1>Página de Faq</h1> );
+}
+ 
+export default Faq;
---
+++ app/tasks/page.tsx
@@ -0,0 +1,5 @@
+const Tasks = () => {
+    return ( <h1>Página de Tarefas</h1> );
+}
+ 
+export default Tasks;
