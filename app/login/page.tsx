961da9f422ba8967191bcb4efe1840931b93ced1
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-12-20 16:44:39 -0300

    setup project prisma

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,22 @@
+import { Button } from "../_components/ui/button";
+
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+    <div className="flex flex-col gap-4 justify-center h-full w-2/5 px-32">
+      <h1 className="text-3xl font-bold w-">Task Manager</h1>
+
+      <div>
+        <h2 className="text-xl font-bold">Bem-Vindo</h2>
+        <p className="text-sm text-slate-600">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
+      </div>
+
+      <Button type="submit" variant="default">
+        Fazer login ou Criar conta
+      </Button>
     </div>
   );
 };
