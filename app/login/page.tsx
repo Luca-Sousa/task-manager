b511f8e6b6a7e7cdd2ef0b1b4c6b545389b591ed
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-12-26 09:39:52 -0300

    autenticação com o clerk provider

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,13 @@
+import { LogInIcon } from "lucide-react";
 import { Button } from "../_components/ui/button";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Login = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) return redirect("/");
+
   return (
     <div className="flex h-full w-2/5 flex-col justify-center gap-4 px-32">
       <h1 className="w- text-3xl font-bold">Task Manager</h1>
@@ -10,18 +17,28 @@ const Login = () => {
         <p className="text-sm text-slate-600">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
-          facilitando o controle do seu orçamento.
-          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
-          monitorar suas movimentações, e oferecer insights personalizados,
-          facilitando o controle do seu orçamento.
+          facilitando o controle do seu orçamento. A Finance AI é uma plataforma
+          de gestão financeira que utiliza IA para monitorar suas movimentações,
+          e oferecer insights personalizados, facilitando o controle do seu
+          orçamento.
         </p>
       </div>
 
-      <Button type="submit" variant="default">
-        Fazer login ou Criar conta
-      </Button>
+      <SignInButton>
+        <Button className="gap-2">
+          <LogInIcon />
+          Fazer login
+        </Button>
+      </SignInButton>
+
+      <SignUpButton>
+        <Button className="gap-2">
+          <LogInIcon />
+          Criar Conta
+        </Button>
+      </SignUpButton>
     </div>
   );
 };
 
-export default Login;
+export default LoginPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-const Dashboard = () => {
-  return <h1>Página de Dashboard</h1>;
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Dashboard = async () => {
+  const { userId } = await auth();
+  if (!userId) return redirect("/login");
+
+  return <UserButton showName />;
 };
 
 export default Dashboard;
