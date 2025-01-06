51e016f3b3479788d2c6281de7a32f318b276540
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-01-06 20:58:50 -0300

    feat: adição de uma tabela de dados das tarefas

---
+++ app/(dashboard)/tasks/_actions/update-task-status/index.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { TasksStatus } from "@prisma/client";
+
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { db } from "@/app/_lib/prisma";
+
+interface UpdateTaskStatusParams {
+  id: string;
+  status: TasksStatus;
+}
+
+export const updateTaskStatus = async ({
+  id,
+  status,
+}: UpdateTaskStatusParams) => {
+  const { userId } = auth();
+  if (!userId) throw new Error("Você não está logado.");
+
+  if (!id && !status) {
+    throw new Error("ID e status são obrigatórios.");
+  }
+
+  await db.tasks.update({
+    where: { id },
+    data: { status },
+  });
+
+  revalidatePath("/tasks");
+};
--- app/(dashboard)/tasks/_columns/index.tsx
+++ app/(dashboard)/tasks/_columns/index.tsx
@@ -2,7 +2,10 @@
 
 import { Tasks } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import TasksTypeBadge from "./_components/type-badge";
+import TasksTypeBadge from "../_components/type-badge";
+import { Button } from "@/app/_components/ui/button";
+import { ExternalLinkIcon, PencilIcon, Trash2Icon } from "lucide-react";
+import { TASK_CATEGORY_LABELS } from "@/app/_constantes";
 
 export const tasksColumns: ColumnDef<Tasks>[] = [
   {
@@ -12,6 +15,7 @@ export const tasksColumns: ColumnDef<Tasks>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
+    cell: ({ row: { original: task } }) => TASK_CATEGORY_LABELS[task.category],
   },
   {
     accessorKey: "status",
@@ -21,13 +25,50 @@ export const tasksColumns: ColumnDef<Tasks>[] = [
   {
     accessorKey: "startTime",
     header: "Prazo Inicial",
+    cell: ({ row: { original: task } }) =>
+      new Date(task.startTime).getHours() +
+      ":" +
+      new Date(task.startTime).getMinutes().toString().padStart(2, "0") +
+      ", " +
+      new Date(task.endTime).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
   },
   {
     accessorKey: "endTime",
     header: "Prazo Final",
+    cell: ({ row: { original: task } }) =>
+      new Date(task.endTime).getHours() +
+      ":" +
+      task.startTime.getMinutes().toString().padStart(2, "0") +
+      ", " +
+      new Date(task.endTime).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
   },
   {
     accessorKey: "actions",
-    header: "",
+    header: "Ações",
+    cell: () => {
+      return (
+        <div className="space-x-0.5">
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <ExternalLinkIcon />
+          </Button>
+
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <PencilIcon />
+          </Button>
+
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <Trash2Icon />
+          </Button>
+        </div>
+      );
+    },
   },
 ];
