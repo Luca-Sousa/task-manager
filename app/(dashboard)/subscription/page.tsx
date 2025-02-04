dc0bfb8d3c4acd393ba68336d192122fbf6e50c4
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-02-04 10:47:17 -0300

    feat: add the subscription plans component

--- app/(dashboard)/subscription/page.tsx
+++ app/(dashboard)/subscription/page.tsx
@@ -6,9 +6,19 @@ import {
   BreadcrumbSeparator,
   BreadcrumbPage,
 } from "@/app/_components/ui/breadcrumb";
+import { Button } from "@/app/_components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/app/_components/ui/card";
 import { Separator } from "@/app/_components/ui/separator";
 import { SidebarInset, SidebarTrigger } from "@/app/_components/ui/sidebar";
 import { auth } from "@clerk/nextjs/server";
+import { CheckIcon } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 const SubscriptionPage = async () => {
@@ -34,13 +44,109 @@ const SubscriptionPage = async () => {
           </Breadcrumb>
         </div>
       </header>
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50"></div>
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+
+      <div className="flex min-h-[100vh] flex-1 flex-col p-4 lg:min-h-min">
+        <Card className="flex h-full flex-col justify-between">
+          <CardHeader className="mx-auto max-w-4xl text-center">
+            <CardTitle className="text-balance text-2xl font-semibold tracking-tight sm:text-4xl">
+              Choose the right plan for you
+            </CardTitle>
+            <CardDescription className="mx-auto max-w-2xl text-pretty text-center text-lg font-medium sm:text-xl">
+              Choose an affordable plan that’s packed with the best features for
+              engaging your audience, creating customer loyalty, and driving
+              sales.
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="mx-auto grid max-w-lg flex-1 grid-cols-1 items-center gap-y-6 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
+            <div className="rounded-3xl bg-accent-foreground/90 p-8 sm:mx-8 sm:rounded-b-none sm:p-10 lg:mx-0 lg:rounded-bl-3xl lg:rounded-tr-none">
+              <h1 className="text-base/7 font-bold text-primary">Hobby</h1>
+
+              <p className="mt-4 flex items-baseline gap-x-2">
+                <span className="text-3xl font-semibold tracking-tight text-gray-900">
+                  $0
+                </span>
+                <span className="text-base/7 text-gray-500">/month</span>
+              </p>
+
+              <p className="mt-6 text-base/7 text-gray-500">
+                The perfect plan if you&#039;re just getting started with our
+                product.
+              </p>
+
+              <ul
+                role="list"
+                className="mt-8 space-y-3 text-sm/6 text-gray-600 sm:mt-10"
+              >
+                <li className="flex gap-x-3">
+                  <CheckIcon className="stroke-primary" />
+                  25 products
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckIcon className="stroke-primary" />
+                  Up to 10,000 subscribers
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckIcon className="stroke-primary" />
+                  Advanced analytics
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckIcon className="stroke-primary" />
+                  24-hour support response time
+                </li>
+              </ul>
+            </div>
+
+            <div className="relative rounded-3xl border border-muted-foreground/40 bg-muted-foreground/10 p-8 shadow-2xl sm:p-10">
+              <h1 className="text-base/7 font-bold text-primary">Enterprise</h1>
+
+              <p className="mt-4 flex items-baseline gap-x-2">
+                <span className="text-5xl font-semibold tracking-tight text-white">
+                  $99
+                </span>
+                <span className="text-base text-gray-400">/month</span>
+              </p>
+
+              <p className="mt-6 text-base/7 text-gray-300">
+                Dedicated support and infrastructure for your company.
+              </p>
+
+              <ul
+                role="list"
+                className="mt-8 space-y-3 text-sm/6 text-gray-300 sm:mt-10"
+              >
+                <li className="flex gap-x-3">
+                  <CheckIcon className="stroke-primary" />
+                  Unlimited products
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckIcon className="stroke-primary" />
+                  Unlimited subscribers
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckIcon className="stroke-primary" />
+                  Advanced analytics
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckIcon className="stroke-primary" />
+                  Dedicated support representative
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckIcon className="stroke-primary" />
+                  Marketing automations
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckIcon className="stroke-primary" />
+                  Custom integrations
+                </li>
+              </ul>
+
+              <Button className="shadow-xs mt-8 w-full" asChild>
+                <Link href="#">Get started today</Link>
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </SidebarInset>
   );
