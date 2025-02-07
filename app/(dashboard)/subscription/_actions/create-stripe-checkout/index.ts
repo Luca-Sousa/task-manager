9f81217ed9b08366eb21a062aec602efcf9cd7ba
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-02-07 15:35:47 -0300

    fix: ensure user ir properly redirected after stripe checkout

--- app/(dashboard)/subscription/_actions/create-stripe-checkout/index.ts
+++ app/(dashboard)/subscription/_actions/create-stripe-checkout/index.ts
@@ -17,8 +17,8 @@ export const createStripeCheckout = async () => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "subscription",
-    success_url: "http://localhost:3000/subscription",
-    cancel_url: "http://localhost:3000/subscription",
+    success_url: process.env.API_URL,
+    cancel_url: process.env.API_URL,
     subscription_data: {
       metadata: {
         clerk_user_id: userId,
